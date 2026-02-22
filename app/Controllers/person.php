da525453e31ff4e3c8f28262443d6bfd7aafcef9
irishann22 <adriasirishann7@gmail.com>
2026-02-22 15:14:32 +0800

    Irish: Added Update part

--- app/Controllers/person.php
+++ app/Controllers/person.php
@@ -35,34 +35,31 @@ class Person extends Controller
         }
     }
 
+ public function update(){
+    $model = new PersonModel(); 
+    $logModel = new LogModel();
 
-    public function update(){
-        $model = new UserModel();
-        $logModel = new LogModel();
-        $userId = $this->request->getPost('id');
-        $name = $this->request->getPost('name');
-        $bday = $this->request->getPost('bday');
+    $userId = $this->request->getPost('id');
 
-        $userData = [
-            'name'       => $name,
-            'bday'      => $bday,
-        ];
+    $data = [
+        'name' => $this->request->getPost('name'),
+        'bday' => $this->request->getPost('bday'),
+    ];
 
-        $updated = $model->update($userId, $userData);
+    if ($model->update($userId, $data)) {
+        $logModel->addLog('Person updated: ' . $data['name'], 'UPDATED');
 
-        if ($updated) {
-            $logModel->addLog('New Person has been apdated: ' . $name, 'UPDATED');
-            return $this->response->setJSON([
-                'success' => true,
-                'message' => 'Person updated successfully.'
-            ]);
-        } else {
-            return $this->response->setJSON([
-                'success' => false,
-                'message' => 'Error updating user.'
-            ]);
-        }
+        return $this->response->setJSON([
+            'success' => true,
+            'message' => 'Person updated successfully.'
+        ]);
     }
+
+    return $this->response->setJSON([
+        'success' => false,
+        'message' => 'Error updating person.'
+    ]);
+}
 
     public function edit($id){
         $model = new PersonModel();
@@ -74,7 +71,7 @@ class Person extends Controller
         return $this->response->setStatusCode(404)->setJSON(['error' => 'User not found']);
     }
 }
-
+ 
 public function delete($id){
     $model = new PersonModel();
     $logModel = new LogModel();
